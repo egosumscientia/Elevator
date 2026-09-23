510fc475e00686d8b61013e46feb326d07591a34
egosumscientia <egosumscientia@gmail.com>
2026-09-23 18:08:29 -0500

    phase 1 finished

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-
 from src.elevator.building import Building
 from src.elevator.elevator import Elevator
 
-
-building01 = Building(1,5)
-print(building01.is_valid_floor(4))
-
+building01 = Building(1, 3)
 elevator01 = Elevator(building01)
-print(elevator01.moving)
-print(elevator01.current_floor)
+
+print(elevator01.current_floor, elevator01.moving)  # 1 False
+
+elevator01.move_up()
+elevator01.move_up()
+print(elevator01.current_floor, elevator01.moving)  # 3 False
+
+elevator01.move_down()
+print(elevator01.current_floor, elevator01.moving)  # 2 False
+
+elevator01.move_down()
+print(elevator01.current_floor, elevator01.moving)  # 1 False
+
+elevator01.move_down()  # ValueError esperado: ya está en el piso mínimo
--- src/elevator/elevator.py
+++ src/elevator/elevator.py
@@ -4,3 +4,18 @@ class Elevator:
         self.current_floor = building.lowest_floor
         self.moving = False
 
+    def move_up(self):
+        if self.current_floor == self.building.highest_floor:
+            raise ValueError("The elevator is already at the highest available floor.")
+        else:
+            self.moving = True
+            self.current_floor += 1
+            self.moving = False
+
+    def move_down(self):
+        if self.current_floor == self.building.lowest_floor:
+            raise ValueError("The elevator is already at the lowest available floor.")
+        else:
+            self.moving = True
+            self.current_floor -= 1
+            self.moving = False
